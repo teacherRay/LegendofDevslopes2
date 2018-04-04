b006ecdc49ab767ee763eb860452a3cc06b5c84b
teacherRay <34903301+teacherRay@users.noreply.github.com>
2018-04-04 19:48:08 +0700

    Ver.1.9
    Fighting and player health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+    [SerializeField] int startingHealth = 100;
+    [SerializeField] float timeSinceLastHit = 2f;
+
+    private float timer = 0f;
+    private CharacterController characterController;
+    private Animator anim;
+    private int currentHealth;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        currentHealth = startingHealth;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+    if (timer >= timeSinceLastHit && !GameManager.instance.GameOver)
+        {
+            if(other.tag == "Weapon")
+            {
+                takehit();
+                timer = 0;
+            }
+        }
+
+    }
+    void takehit()
+    {
+        if (currentHealth > 0)
+        {
+            GameManager.instance.PlayerHit(currentHealth);
+            anim.Play("Hurt");
+            currentHealth -= 10;
+        }
+
+        if (currentHealth <= 0)
+        {
+            killPlayer();
+        }
+    }
+        void killPlayer()
+        {
+            GameManager.instance.PlayerHit(currentHealth);
+            anim.SetTrigger("HeroDie");
+            characterController.enabled = false;
+        }
+    }
+
+
+
